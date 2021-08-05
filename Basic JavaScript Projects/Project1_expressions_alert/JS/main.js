6a21c2c4f9d7a51ffbe2d3bb540793ca70456bd6
Gabe Larson <gabriel.g.larson@gmail.com>
2021-08-04 20:31:44 -0700

    First day of practice
    Tried out different things using JavaScript in a basic html format. Good chance that the code will completely change come tomorrow but a small part of today will be saved in this version

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var Family = "The Smiths", Dad = "Steve", Mom = "Sally",
+Daughter = "Gale", Son = "Clark";
+
+document.write(Son);
+
+var Family = Family.fontcolor("green");
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("red");
+var Daughter = Daughter.fontcolor("purple");
+var Son = Son.fontcolor("yellow");
+
+document.write(Mom);
+document.write(Son);
