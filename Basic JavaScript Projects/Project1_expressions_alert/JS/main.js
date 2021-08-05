aac4a8ccbeffa369438ecb22a493838c1aa1ffbe
Gabe Larson <gabriel.g.larson@gmail.com>
2021-08-05 10:16:26 -0700

    Update main.js
    All the requirements for the basic JavaScript projects assignment. Had to redo some things that I deleted thinking I wouldn't need them again.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,17 @@
+window.alert("This is an alert"); //This makes an alert with a button
+
+var A = "water";  //assign a string to a variable
+var B = "melon"; //assign a string to a variable
+
+document.write(A+B); //concatenate variables together
+
+
 var Family = "The Smiths", Dad = "Steve", Mom = "Sally",
-Daughter = "Gale", Son = "Clark";
+Daughter = "Gale", Son = "Clark";  //multiple variables
 
 document.write(Son);
 
-var Family = Family.fontcolor("green");
+var Family = Family.fontcolor("green"); //fontcolors
 var Dad = Dad.fontcolor("blue");
 var Mom = Mom.fontcolor("red");
 var Daughter = Daughter.fontcolor("purple");
@@ -11,3 +19,5 @@ var Son = Son.fontcolor("yellow");
 
 document.write(Mom);
 document.write(Son);
+
+document.write(4+3);  //an expression is written//
